6b286801aeca14bde52a8abc4016a4c0ed56cd50
Agus Tjakra <49585004+gsdiredja@users.noreply.github.com>
2026-07-15 22:19:49 +0700

    Create datasiswa.js

---
+++ datasiswa.js
@@ -0,0 +1,6 @@
+// datasiswa.js
+const daftarSiswa = [
+    { username: "siswa1", password: "123", nama: "Budi Santoso", kelas: "XI-1" },
+    { username: "siswa2", password: "123", nama: "Siti Rahma", kelas: "XI-1" },
+    { username: "siswa3", password: "123", nama: "Ahmad Fauzi", kelas: "XI-1" }
+];
