2d6697f9e21941189aaf4d1d59a8a0acd97d363d
Agus Tjakra <49585004+gsdiredja@users.noreply.github.com>
2026-07-21 22:16:36 +0700

    Update config.js

--- guru/config.js
+++ guru/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyM9h-iUZEd8YU-AAAXRdrgc09YB2gnBypHlYf0kUXcK1J1oI03q_5Dqzt5MJnPtX-X/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyKp1QwU5t2gq-_exU1vLPAR9V4R4dIoB6e8Ltj4aND-x9yEeHY3FfZxwlNyslBJcY5/exec";
