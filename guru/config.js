4953d38dcf6f9efa46af8566dc64a5e6746bc7fa
Agus Tjakra <49585004+gsdiredja@users.noreply.github.com>
2026-07-25 02:48:22 +0700

    Update config.js

--- guru/config.js
+++ guru/config.js
@@ -1,10 +1,8 @@
 // config.js
-// URL Web App dari Google Apps Script Anda (WAJIB berakhiran /exec)
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxB9i1MzUJh7xuJYWq9RycwQ9_J7OHcmH49IVbIlHHyoVsjSdKP75_I_xN8yQo_qNTT/exec";
 
-// Menyiapkan variabel global agar aman dipanggil dari halaman manapun
 window.SCRIPT_URL = SCRIPT_URL;
-window.API_URL = SCRIPT_URL; // Alias variabel untuk kompatibilitas penuh
+window.API_URL = SCRIPT_URL;
 
 window.CONFIG = {
     SCRIPT_URL: SCRIPT_URL,
